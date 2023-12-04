616f26befbb6f2211aadeb9a166570cb321dc470
Rasche0809 <90986838+Rasche0809@users.noreply.github.com>
2023-12-05 00:51:38 +0100

    feat: Implement input error handling in Extensions
    Added a new method TryParseToInt in Extensions class, which
    includes an optional parameter checkOneOrTwo. This method enhances
    input validation by checking if the input string is a valid integer
    and optionally verifies if it is either 1 or 2. This update provides
    robust error handling for user inputs, ensuring that the application
    can gracefully manage incorrect or unexpected input values.

---
+++ AoC2023.Presentation/Extensions.cs
@@ -0,0 +1,17 @@
+﻿namespace AoC23.Presentation;
+
+public static class Extensions
+{
+    public static bool TryParseToInt(this string input, out int result, bool checkOneOrTwo = false)
+    {
+        result = 0;
+        bool parseSuccess = int.TryParse(input, out result);
+
+        if (checkOneOrTwo)
+        {
+            return parseSuccess && (result == 1 || result == 2);
+        }
+
+        return parseSuccess;
+    }
+}
--- AoC2023.Presentation/Program.cs
+++ AoC2023.Presentation/Program.cs
@@ -8,15 +8,29 @@ class Program
     {
         Console.WriteLine("Advent of Code 2023");
         Console.WriteLine("Bitte wählen Sie den Tag (1-24):");
-        int day = int.Parse(Console.ReadLine());
-        Console.WriteLine("Bitte wählen Sie den Teil (1-2):");
-        int part = int.Parse(Console.ReadLine());
-        FileDialogService fileDialogService = new();
-        string filePath = fileDialogService.GetFilePath();       
+        var user_day = Console.ReadLine();
+        if(!user_day.TryParseToInt(out int day, false))
+            Console.WriteLine("Keine Zahl");
         var calculator = DayCalculatorFactory.GetCalculator(day);
         if (calculator is null)
+        {
             Console.WriteLine("Nicht verfügbar!");
+            return;
+        }
+        Console.WriteLine("Bitte wählen Sie den Teil (1-2):");
+        var user_part = Console.ReadLine();
+        user_day.TryParseToInt(out int part, true);
+        if (part != 1 && part != 2)
+        {
+            Console.WriteLine("Nicht verfügbar!");
+            return;
+        }
+
+        FileDialogService fileDialogService = new();
+        string filePath = fileDialogService.GetFilePath();       
+        
         var result = part == 1 ? calculator!.CalculatePart1(filePath) : calculator!.CalculatePart2(filePath);
         Console.WriteLine($"Ergebnis für Tag {day}, Teil {part}: {result}");
+        Console.ReadKey();
     }
 }
